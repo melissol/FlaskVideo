9a72012028199c938c15d7004f9477ede63a50bb
Loukas <s9969671@stud.uni-frankfurt.de>
2023-06-02 10:28:27 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_compress import Compress
+
+app = Flask(__name__, static_folder='static')
+Compress(app)
+app.config['COMPRESS_COMPRESSION_LEVEL'] = 9
+
+
+@app.route('/')
+def index():
+    return render_template('video.html')
+
+
+@app.route('/video_buffering')
+def video_buff():
+    return render_template('video_buff.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
