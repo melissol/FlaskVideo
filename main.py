f6b369297c81cdefaa3527ca1810ff60a1faf1e8
Loukas <s9969671@stud.uni-frankfurt.de>
2023-06-02 23:08:52 +0200

    updated files

--- main.py
+++ main.py
@@ -6,11 +6,6 @@ Compress(app)
 app.config['COMPRESS_COMPRESSION_LEVEL'] = 9
 
 
-# @app.route('/')
-# def index():
-#    return render_template('home.html')
-
-
 @app.route('/')
 def video():
     return render_template('video.html')
@@ -22,5 +17,4 @@ def video_buff():
 
 
 if __name__ == '__main__':
-    """ """
     app.run(debug=True)
